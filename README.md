a79eefba6597aea11cfd00f69e6a1d48bb0da9d1
IndigoTeddy13 <90729867+IndigoTeddy13@users.noreply.github.com>
2025-03-02 21:09:48 -0500

    Adding slightly modified version of rofi-favorites script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CachyDotfiles
 
 How I configure my bash shell (among other things) for [CachyOS](./prep.md).
+
+# Scripts from Others (aside from basic config files)
+
+- [Rofi-Favorites](https://github.com/luiscrjunior/rofi-favorites)
---
+++ dotfiles/.config/rofi/rofi-favorites.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Source: https://github.com/luiscrjunior/rofi-favorites
+
+GET_FAVORITES_CMD="gsettings get org.gnome.shell favorite-apps"
+DESKTOP_FILES_PATH="/usr/share/applications/"
+FLATPAK_FILES_PATH="/var/lib/flatpak/exports/share/applications/"
+
+$GET_FAVORITES_CMD | grep -Po "\'(.+?)\'" | sed -r "s/'$|^'//g" | while read -r favorite ; do
+    desktop_file="${DESKTOP_FILES_PATH}${favorite}"
+    flatpak_desktop_file="${FLATPAK_FILES_PATH}${favorite}"
+
+    if [ -f "$desktop_file" ]; then
+        name=$(cat $desktop_file | awk -F "=" '/Name=/ {print $2}' | head -1)
+        command=$(cat $desktop_file | awk -F "=" '/Exec=/ {print $2}' | head -1)
+        icon=$(cat $desktop_file | awk -F "=" '/Icon=/ {print $2}' | head -1)
+    elif [ -f "$flatpak_desktop_file" ]; then
+        name=$(cat $flatpak_desktop_file | awk -F "=" '/Name=/ {print $2}' | head -1)
+        command=$(cat $flatpak_desktop_file | awk -F "=" '/Exec=/ {print $2}' | head -1)
+        icon=$(cat $flatpak_desktop_file | awk -F "=" '/Icon=/ {print $2}' | head -1)
+    else
+        continue
+    fi
+
+    if [ $# -eq 0 ]; then
+      echo -en "${name}\0icon\x1f${icon}\n"
+    fi
+
+    if [ $# -eq 1 ]; then
+        chosen="$1"
+        if [ "$chosen" == "$name" ]; then
+            coproc gtk-launch ${favorite}
+            exit
+        fi
+    fi
+
+done
